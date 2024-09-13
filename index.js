00a36686e5c9534dd847b7a628bdcf8387178c60
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2024-09-12 23:34:52 -0500

    create and setup conversion functions + btn function

--- index.js
+++ index.js
@@ -3,3 +3,33 @@ let volume = document.getElementById('volume')
 let mass = document.getElementById('mass')
 let length = document.getElementById('length')
 let number = document.getElementById("number")
+
+btn.addEventListener("click", function(input){
+    volume.innerText = convertVolume(number)
+    length.innerText = convertLength(number)
+    mass.innerText = convertMass(number)
+})
+
+function convertLength(input) {
+    let feetResult = input.value / 3.281
+    let meterResult = input.value * 3.281
+
+    return `${input.value} meters = ${feetResult.toFixed(3)} feet | ${input.value} feet = ${meterResult.toFixed(3)} meters`
+
+}
+
+function convertVolume(input) {
+    let literResult = input.value / 0.264
+    let gallonResult = input.value * 3.785
+
+    return `${input.value} liters = ${gallonResult.toFixed(3)} gallons | ${input.value} gallons = ${literResult.toFixed(3)} liters`
+
+}
+
+function convertMass(input) {
+    let kiloResult = input.value / 2.204
+    let poundResult = input.value * 2.204
+
+    return `${input.value} kilos = ${poundResult.toFixed(3)} pounds | ${input.value} pounds = ${kiloResult.toFixed(3)} kilos`
+
+}
